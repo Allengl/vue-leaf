79f650b928ac94641dc6fb4c82e6052afac41099
dengl <guolong613@gmail.com>
2023-02-24 04:24:37 +0000

    添加 deploy_to_github.sh

---
+++ bin/deploy_to_github.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env/ bash
+rm -rf dist
+npm run build
+cd dist
+git init
+git add .
+git commit -m deploy
+git remote add origin git@github.com:Allengl/vue-leaf-preview.git
+git branch -M master
+git push -f origin master:master
+cd -
